9b6f04649be6af7c9b1ce2902e9a26070086bea8
zhengwenhao <931516643@qq.com>
2022-06-28 08:42:12 +0800

    feat(add): err

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import './styles/index.scss'
+const app = createApp(App)
+app.use(store).use(router).use(ElementPlus).mount('#app')
